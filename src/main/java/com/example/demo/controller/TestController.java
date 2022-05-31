8b902d5977c6fe718f7ca71b50f2f32ca48d6cb3
sungmin83 <dreamfctory@naver.com>
2022-05-31 15:56:55 +0900

    Add  - ResponseBody  - ResponseEntity

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/test")
+@RequestMapping("test")
 
 public class TestController {
 	@GetMapping
@@ -29,4 +36,26 @@ public class TestController {
 	public String testControllerRequestParam(@RequestParam(required = false) int id) {
 		return "Hello World! ID : " + id;
 	}
+	
+	@GetMapping("testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID : " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+	}
+	
+	@GetMapping("testResponseBody")
+	public ResponseDTO<String> testControllerResponseBody(){
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! I'm ResponseDTO");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return response;
+	}
+	
+	@GetMapping("testResponseEntity")
+	public ResponseEntity<?> testResponseEntity(){
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! I'm ResponseEntity. And you got 400!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.badRequest().body(response);
+	}
+	
 }
