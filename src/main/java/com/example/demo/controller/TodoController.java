3fe61f6d93f6c22a908dacccfa199632ac363c8d
sungmin <sungmin@218.38.137.28>
2022-06-01 14:59:40 +0900

    Update TodoController.java

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,14 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
+//import com.example.demo.dto.TodoDTO;
+//import com.example.demo.model.TodoEntity;
+//import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+//import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
-	@GetMapping
-	public String testTodo() {
-		return "Hello Todo World!";
+	
+	@Autowired
+	//private TodoService service;
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testTodo(){
+		List<String> list = new ArrayList<>();
+		list.add("todoResponseEntity");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
 	}
+	
 }
