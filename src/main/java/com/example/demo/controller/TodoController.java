142f17782735f195a32f28abd372334fdee270fb
sungmin83 <dreamfctory@naver.com>
2022-06-02 18:59:58 +0900

    Add CreateTodo

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,23 +2,22 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.service.TodoService;
-
-//import com.example.demo.dto.TodoDTO;
-//import com.example.demo.model.TodoEntity;
-//import com.example.demo.service.TodoService;
+import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
+import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.stream.Collectors;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -36,4 +35,36 @@ public class TodoController {
 		return ResponseEntity.ok().body(response);
 	}
 	
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+		try {
+			String temporaryUserId = "temporary-user"; //temporary user id
+			
+			//TodoEntity로 변환
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			//id를 null로 초기화. 생성 당시에는 id가 없어야 하기 때문
+			entity.setId(null);
+			
+			//임시 사용자 아이디 설정. 
+			entity.setUserId(temporaryUserId);
+			
+			//서비스를 이용해 Todo 엔티티 생성
+			List<TodoEntity> entities = service.create(entity);
+			
+			//자바스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			//변환된 TodoDTO 리스트릴 이용해 ResponseDTO 초기화
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			//ResponseDTO를 리턴
+			return ResponseEntity.ok().body(response);
+			
+		} catch(Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,10 +1,16 @@
 package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @Service
 public class TodoService {
 	
@@ -19,5 +25,26 @@ public class TodoService {
 		//TodoEntoty 검색
 		TodoEntity savedEntity = repository.findById(entity.getId()).get();
 		return savedEntity.getTitle();
+	}
+	
+	
+	private void validate(final TodoEntity entity) {
+		if(entity == null) {
+			log.warn("Entity cannot be null");
+			throw new RuntimeException("Entity cannot be null");
+		}
+		
+		if(entity.getUserId() == null) {
+			log.warn("Unknown user");
+			throw new RuntimeException("Unknown user");
+		}
+	}
+	
+	public List<TodoEntity> create(final TodoEntity entity){
+		repository.save(entity);
+		
+		log.info("Entity Id : {} is saved", entity.getId());
+		
+		return repository.findByUserId(entity.getUserId());
 	}
 }
