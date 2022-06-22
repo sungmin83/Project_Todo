e2991869abe9aa5118f85c833d759c22c7be85e5
sungmin83 <dreamfctory@naver.com>
2022-06-22 20:09:52 +0900

    Add - User Layer

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.model.UserEntity;
+import com.example.demo.persistence.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public UserEntity create(final UserEntity userEntity) {
+		if(userEntity == null || userEntity.getEmail() == null) {
+			throw new RuntimeException("Invalid arguments");
+		}
+		final String email = userEntity.getEmail();
+		if(userRepository.existsByEmail(email)) {
+			log.warn("Email already exists {}", email);
+		}
+		
+		return userRepository.save(userEntity);
+	}
+	
+	public UserEntity getByCredentials(final String email, final String password) {
+		return userRepository.findByEmailAndPassword(email, password);
+	}	
+}
