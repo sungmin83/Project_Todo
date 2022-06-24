0afd984dda1d34461292a9a7f5b7b579b9d602e2
sungmin83 <dreamfctory@naver.com>
2022-06-24 19:19:53 +0900

    add TokenProvider update build.gradle

---
+++ src/main/java/com/example/demo/security/TokenProvider.java
@@ -0,0 +1,44 @@
+package com.example.demo.security;
+
+import com.example.demo.model.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+
+@Slf4j
+@Service
+public class TokenProvider {
+	private static final String SECRET_KEY = "NMA8JPctFuna59f5";
+	
+	public String create(UserEntity userEntity) {
+		//기한을 지금부터 1일로 설정
+		Date expiryDate = Date.from(
+				Instant.now()
+				.plus(1, ChronoUnit.DAYS));
+		
+		// JWT Token 생성
+		return Jwts.builder()
+				.signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+				.setSubject(userEntity.getId())
+				.setIssuer("demo app")
+				.setIssuedAt(new Date())
+				.setExpiration(expiryDate)
+				.compact();
+	}
+	
+	public String validateAndGetUserId(String token) {
+		Claims claims = Jwts.parser()
+				.setSigningKey(SECRET_KEY)
+				.parseClaimsJws(token)
+				.getBody();
+		
+		return claims.getSubject();
+	}
+}
